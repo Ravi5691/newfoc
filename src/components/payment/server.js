6a1b73899839e5296bbf440963839372c6a9b74a
Ravinder Singh Bisht <ravinderbisht5691@gmail.com>
2024-11-18 13:21:26 +0530

    PORT

--- src/components/payment/server.js
+++ src/components/payment/server.js
@@ -31,7 +31,7 @@ app.post("/api/payment/order", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
